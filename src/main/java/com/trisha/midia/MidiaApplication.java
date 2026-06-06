75a2f84f23beb86f663c47e2591cab6896622fb9
rafae <rafael.santoss2802@gmail.com>
2026-06-06 11:11:54 -0300

    Initial commit

---
+++ src/main/java/com/trisha/midia/MidiaApplication.java
@@ -0,0 +1,13 @@
+package com.trisha.midia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trisha/midia/MidiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trisha.midia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
